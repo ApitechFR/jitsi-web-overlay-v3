fbb83551c2869337e74acc5f49612184b85c8d8a
moukmessie <moukmessie@gmail.com>
2025-06-27 15:55:11 +0200

    update import path for ConferenceModule in authentication.module.ts

--- jitsi-web-overlay-backend/src/authentication/authentication.module.ts
+++ jitsi-web-overlay-backend/src/authentication/authentication.module.ts
@@ -2,7 +2,7 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
-import { ConferenceModule } from 'src/conference/conference.module';
+import { ConferenceModule } from '../conference/conference.module';
 
 @Module({
   imports: [HttpModule, ConferenceModule],
