be7c69f11b90814e09a0530ab810da25030c6073
Florent Tirvaudey <florentt@apitech.fr>
2025-07-03 11:42:17 +0200

    feat: add stars for rating in feedback

---
+++ jitsi-web-overlay-frontend/src/components/joona/svgDef/SvgDefs.tsx
@@ -0,0 +1,13 @@
+
+function SvgDefs () {
+    
+  return (
+    <svg style={{ display: "none" }}>
+      <symbol id="star" viewBox="0 0 24 24">
+        <path d="M12 2l3 7h7l-5.5 4.5 2 7-6-4-6 4 2-7L2 9h7z" />
+      </symbol>
+    </svg>
+  );
+};
+
+export default SvgDefs;
