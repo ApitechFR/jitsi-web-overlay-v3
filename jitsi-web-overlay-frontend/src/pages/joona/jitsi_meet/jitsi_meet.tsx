e1d018c6c45dc7f40ead89bd19b46caead439e3c
moukmessie <moukmessie@gmail.com>
2025-08-11 10:38:29 +0200

    chore: clean up unused imports and improve code readability

--- jitsi-web-overlay-frontend/src/pages/joona/jitsi_meet/jitsi_meet.tsx
+++ jitsi-web-overlay-frontend/src/pages/joona/jitsi_meet/jitsi_meet.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import Swal from 'sweetalert2';
 import { validateRoomName } from '../../../utils/roomName';
