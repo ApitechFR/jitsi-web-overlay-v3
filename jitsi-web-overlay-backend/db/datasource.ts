fdeec9723223dec0c1cb28fbac1ba081625fa401
mahfoudh <mahfoud.hafdi@apitech.fr>
2025-08-14 15:14:42 +0200

    datasource configuration to use ConfigService for environment variables

--- jitsi-web-overlay-backend/db/datasource.ts
+++ jitsi-web-overlay-backend/db/datasource.ts
@@ -1,24 +1,27 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import * as dotenv from 'dotenv';
+import { ConfigService } from '@nestjs/config';
 
 dotenv.config();
 
-const production = process.env.NODE_ENV === 'production';
+const configService = new ConfigService();
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 export const dataSourceOptions: DataSourceOptions = {
     type: 'mariadb',
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT || '3306', 10),
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: production ? ['dist/**/*.entity.js'] : ['src/**/*.entity.ts'],
+    host: configService.get<string>('DB_HOST'),
+    port: configService.get<number>('DB_PORT'),
+    username: configService.get<string>('DB_USERNAME'),
+    password: configService.get<string>('DB_PASSWORD'),
+    database: configService.get<string>('DB_NAME'),
+    entities: isProduction ? ['dist/**/*.entity.js'] : ['src/**/*.entity.ts'],
     // Migrations seulement en prod
-    migrations: production ? ['dist/db/migrations/*.js'] : [],
+    migrations: isProduction ? ['dist/db/migrations/*.js'] : [],
     migrationsTableName: 'migrations',
-    migrationsRun: production,
-    synchronize: !production,
-    logging: !production,
+    migrationsRun: isProduction,
+    synchronize: !isProduction,
+    logging: !isProduction,
 };
 
 const dataSource = new DataSource(dataSourceOptions);
