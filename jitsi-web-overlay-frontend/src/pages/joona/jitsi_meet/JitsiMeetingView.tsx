de767e5850333cfb69dcb7bed1931ffad74514df
mahfoudh <mahfoud.hafdi@apitech.fr>
2025-08-22 14:18:30 +0200

    replace jitsiMeeting-1 by root

--- jitsi-web-overlay-frontend/src/pages/joona/jitsi_meet/JitsiMeetingView.tsx
+++ jitsi-web-overlay-frontend/src/pages/joona/jitsi_meet/JitsiMeetingView.tsx
@@ -49,10 +49,10 @@ const JitsiMeetingView: React.FC<Props> = ({ domain, roomName, jwt, displayName 
         console.log('[Jitsi] API prête');
 
         const checkInterval = setInterval(() => {
-          const iframeContainer = document.getElementById("jitsiMeeting-1");
+          const iframeContainer = document.getElementById("root");
 
           if (iframeContainer?.children[0]) {
-            const iframe : any = iframeContainer.children[0];
+            const iframe : any = iframeContainer.children[0].children[0];
 
             if (iframe.contentWindow.document) {
               clearInterval(checkInterval);
