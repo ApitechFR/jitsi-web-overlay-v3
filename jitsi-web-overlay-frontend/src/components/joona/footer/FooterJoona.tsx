3c2a3a72d9c749152ac8f1a53e30414c0f5f71b8
Florent Tirvaudey <florentt@apitech.fr>
2025-09-18 15:12:08 +0200

    hotfix: force logo in footer

--- jitsi-web-overlay-frontend/src/components/joona/footer/FooterJoona.tsx
+++ jitsi-web-overlay-frontend/src/components/joona/footer/FooterJoona.tsx
@@ -1,5 +1,7 @@
 
 import { Footer, FooterProps } from "@apitechfr/react-dsapitech/Footer"
+import apitechLogoLightMode from '/assets/apitech-logo-bleunuit.png'
+import apitechLogoDarkMode from '/assets/apitech-logo-blanc.png'
 
 
 interface props {
@@ -9,6 +11,8 @@ interface props {
 function FooterJoona({ headerFooterDisplayItem }: props) {
   return (
     <Footer
+      mainLogoURL={apitechLogoLightMode}
+      mainLogoURLDark={apitechLogoDarkMode}
       accessibility="fully compliant"
       contentDescription="
         Apitech, Éditeur Open Solutions  - L’esprit du Libre au service de la productivité.
