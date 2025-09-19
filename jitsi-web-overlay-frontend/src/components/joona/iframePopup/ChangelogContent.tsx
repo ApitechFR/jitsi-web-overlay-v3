0299345dc5de21606e0951ab38926209d743a5e4
Florent Tirvaudey <florentt@apitech.fr>
2025-09-19 12:35:26 +0200

    fix: buttons in popup

--- jitsi-web-overlay-frontend/src/components/joona/iframePopup/ChangelogContent.tsx
+++ jitsi-web-overlay-frontend/src/components/joona/iframePopup/ChangelogContent.tsx
@@ -9,33 +9,44 @@ function ChangelogContent({ content }: any) {
         <div className={styles.popupBoxContent}>
             <div className={styles.boxFrame}>
                 <h1>{content.title}</h1>
-                <p>{content.blocks[0].description}</p>
-                <ul>
-                    {content.blocks[0].features.ameliorations.map((data: string) => (
-                        <li key={data}>{data}</li>
-                    ))}
-                </ul>
-                <p>{content.version}</p>
-                {content.blocks[0].link.href && (
-                    <div className={styles.frameContainerButton}>
-                        <Button
-                            className={styles.buttonMoreInfo}
-                            onClick={() => window.location.href = content.blocks[0].link.href}
-                        >
-                            <span>{content.blocks[0].link.label}</span>
-                        </Button>
-                    </div>
-                )}
                 {content.blocks.map((block: any, blockIndex: number) => (
-                    block.link[1]?.link && (
-                        <div className={styles.frameContainerButtonUpload} key={blockIndex}>
-                        <Button
-                            className={styles.buttonMoreInfo}
-                        >
-                            <a href={block.link[1].link} download>{block.link[1].label}</a>
-                        </Button>
+                    <div key={blockIndex}>
+                        <p>{content.blocks[0].description}</p>
+                        {block.features?.ameliorations?.length > 0 && (
+                            <ul>
+                                {block.features.ameliorations.map((data: string) => (
+                                    <li key={data}>{data}</li>
+                                ))}
+                            </ul>
+                        )}
+                        <p>{content.version}</p>
+                        <div className={styles.buttonsContainer}>
+                            {block.link?.map((lien: any, key: number) => (
+                                <div key={key}>
+                                    {lien.href && (
+                                        <div className={styles.frameContainerButton}>
+                                            <Button
+                                            className={styles.buttonMoreInfo}
+                                            onClick={() => (window.location.href = lien.href)}
+                                            >
+                                                <span>{lien.label}</span>
+                                            </Button>
+                                        </div>
+                                    )}
+
+                                    {lien.link && (
+                                        <div className={styles.frameContainerButton}>
+                                            <Button className={styles.buttonMoreInfo}>
+                                                <a href={lien.link} download>
+                                                    {lien.label}
+                                                </a>
+                                            </Button>
+                                        </div>
+                                    )}
+                                </div>
+                            ))}
                         </div>
-                    )
+                    </div>
                 ))}
             </div>
         </div>
