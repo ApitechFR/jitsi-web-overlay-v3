0c5693d4091f39d9beeb7699196e88628b3ab870
moukmessie <moukmessie@gmail.com>
2025-11-07 17:11:13 +0100

    remove admin column addition from migration

--- jitsi-web-overlay-backend/db/migrations/1762266834541-add-admin.ts
+++ jitsi-web-overlay-backend/db/migrations/1762266834541-add-admin.ts
@@ -5,7 +5,6 @@ export class AddAdmin1762266834541 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE \`users\` ADD \`admin\` tinyint NOT NULL DEFAULT 0`);
-        await queryRunner.query(`ALTER TABLE \`users\` ADD \`admin\` tinyint NOT NULL DEFAULT 0`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
