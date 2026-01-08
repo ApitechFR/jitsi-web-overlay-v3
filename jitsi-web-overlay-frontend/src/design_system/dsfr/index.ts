1d26c30e0d5dc2df787cf76dfb8a8dce486e28c8
Florent Tirvaudey <florentt@apitech.fr>
2026-01-08 16:54:29 +0100

    feat: add alias to separate dsapitech and dsfr in visio app

---
+++ jitsi-web-overlay-frontend/src/design_system/dsfr/index.ts
@@ -0,0 +1,1 @@
+export { Input } from '@codegouvfr/react-dsfr/Input';
--- jitsi-web-overlay-frontend/vite.config.ts
+++ jitsi-web-overlay-frontend/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'node:url';
 import { dirname, resolve } from 'node:path';
@@ -6,20 +6,33 @@ import { dirname, resolve } from 'node:path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const DS_MAP = {
+  "joona": resolve(__dirname, "src/design_system/react-dsapitech"),
+  "webconf": resolve(__dirname, "src/design_system/dsfr"),
+};
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/',
-  plugins: [react()],
-  assetsInclude: ['**/*.md'],
-  server: {
-    port: 3000,
-  },
-  build: {
-    outDir: 'dist',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    }
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  const appTemplate = env.VITE_APP_TEMPLATE === 'joona' || env.VITE_APP_TEMPLATE === 'webconf' ? env.VITE_APP_TEMPLATE : 'joona';
+
+  return {
+    base: "/",
+    plugins: [react()],
+    assetsInclude: ["**/*.md"],
+    server: {
+      port: 3000,
+    },
+    build: {
+      outDir: "dist",
+    },
+    resolve: {
+      alias: {
+        "@": resolve(__dirname, "src"),
+        "@ds": DS_MAP[appTemplate]
+      },
+    },
+  };
 });
+
