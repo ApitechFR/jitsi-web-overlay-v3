f213e75a8d9b788a60c9bc279586f2dbc6a731f2
mahfoudh <mahfoud.hafdi@apitech.fr>
2026-01-14 11:56:22 +0100

    ensure timeZone is consistently set to UTC

--- jitsi-web-overlay-frontend/src/utils/date.ts
+++ jitsi-web-overlay-frontend/src/utils/date.ts
@@ -1,10 +1,12 @@
 export const formatDate = (isoString: string): string => {
     console.log('Formatting date:', isoString);
     console.log('returned value:', new Intl.DateTimeFormat('fr-FR', {
+        timeZone: 'UTC',
         dateStyle: 'long',
         timeStyle: 'short',
     }).format(new Date(isoString)));
     return new Intl.DateTimeFormat('fr-FR', {
+        timeZone: 'UTC',
         dateStyle: 'long',
         timeStyle: 'short',
     }).format(new Date(isoString));
