202a39113348abb61ef813587a94b852ef3b1b05
Florent Tirvaudey <florentt@apitech.fr>
2026-01-28 17:24:17 +0100

    feat: add conditional enter modal to webconf and joona

--- jitsi-web-overlay-frontend/src/pages/Home/ConferenceWaitingModal.tsx
+++ jitsi-web-overlay-frontend/src/pages/Home/ConferenceWaitingModal.tsx
@@ -3,13 +3,16 @@ import styles from './HomeJoona.module.css';
 import { useTranslation } from 'react-i18next';
 import { ConferenceNameValidation } from '@/utils/conferenceName';
 
+import { AgentConnectButton, Input, Checkbox, Button } from "@ds";
+
 interface ConferenceWaitingModalProps {
     modal: any;
     authenticated: boolean;
     stopWaitingAndPoll: () => void;
     login: (roomName?: string) => void;
     conferenceName: string;
-    validateConferenceName: (name: string) => ConferenceNameValidation;
+    validateConferenceName: (name: string) => boolean;
+    appTemplate: string;
 }
 
 export const ConferenceWaitingModal: React.FC<ConferenceWaitingModalProps> = ({
@@ -19,6 +22,7 @@ export const ConferenceWaitingModal: React.FC<ConferenceWaitingModalProps> = ({
     login,
     conferenceName,
     validateConferenceName,
+    appTemplate
 }) => {
     const { t } = useTranslation();
     const confNameValid = validateConferenceName(conferenceName);
@@ -26,31 +30,59 @@ export const ConferenceWaitingModal: React.FC<ConferenceWaitingModalProps> = ({
         <modal.Component
             title=""
             concealingBackdrop={false}
-            buttons={[
-                !authenticated && {
-                    children: t('conferenceWaitingModal.authenticate'),
-                    priority: 'primary',
-                    onClick: () => {
-                        stopWaitingAndPoll();
-                        login(confNameValid.isValidConfName ? conferenceName : undefined);
-                    },
-                    doClosesModal: false,
-                },
-                {
-                    children: t('conferenceWaitingModal.cancel'),
-                    priority: 'secondary',
-                    onClick: () => stopWaitingAndPoll(),
-                    doClosesModal: false,
-                },
-            ].filter(Boolean) as any}
+            className={styles.conferenceWaitingModal}
         >
-            <div className={styles.contentModal}>
-                <h1>{t('conferenceWaitingModal.notStartedTitle')}</h1>
-                <p>
-                    {t('conferenceWaitingModal.notStartedMessage.part1')}
-                    <b>Visio By Apitech</b>
-                    {t('conferenceWaitingModal.notStartedMessage.part2')}
-                </p>
+            <div className={styles.enterModal}>
+                <div className={styles.modalMessage}>
+                    <h1>{t('conferenceWaitingModal.organizerTitle')}</h1>
+                    <p>
+                        {t('conferenceWaitingModal.organizerMessage.part1')}
+                        <strong>{conferenceName}</strong>
+                        {t('conferenceWaitingModal.organizerMessage.part2')}
+                    </p>
+                </div>
+                {appTemplate === 'webconf' && (
+                    <>
+                        <AgentConnectButton url="https://google.fr" />
+                        <div className={styles.modalInputBlock}>
+                            <Input
+                            label={t('conferenceWaitingModal.emailLabel')}
+                            id="mailConnection"
+                            />
+                            <Checkbox
+                                options={[
+                                    {
+                                        label: t('conferenceWaitingModal.rememberMe'),
+                                        nativeInputProps: {
+                                            name: 'checkboxes-1',
+                                            value: 'value1'
+                                        }
+                                    }
+                                ]}
+                                state="default"
+                            />
+                            <Button priority="primary" onClick={() => {}}>
+                                <span>{t('conferenceWaitingModal.receiveCode')}</span>
+                            </Button>
+                        </div>
+                    </>
+                )}
+                {appTemplate === 'joona' && !authenticated && (
+                    <Button priority="primary" onClick={() => {
+                        stopWaitingAndPoll();
+                        login(validateConferenceName(conferenceName) ? conferenceName : undefined);
+                    }}>
+                        <span>{t('conferenceWaitingModal.authenticate')}</span>
+                    </Button>
+                )}
+                <div className={styles.modalMessage}>
+                    <h1>{t('conferenceWaitingModal.inviteeTitle')}</h1>
+                    <p>
+                        {t('conferenceWaitingModal.inviteeMessage.part1')}
+                        <strong>{conferenceName}</strong>
+                        {t('conferenceWaitingModal.inviteeMessage.part2')}
+                    </p>
+                </div>
             </div>
         </modal.Component>
     );
--- jitsi-web-overlay-frontend/src/pages/Home/HomeJoona.tsx
+++ jitsi-web-overlay-frontend/src/pages/Home/HomeJoona.tsx
@@ -304,6 +304,7 @@ function HomeJoona(props: HomeJoonaProps) {
         login={login}
         conferenceName={props.conferenceName}
         validateConferenceName={validateConferenceName}
+        appTemplate={AppTemplate}
       />
 
       <div className={styles.firstContainer}>
