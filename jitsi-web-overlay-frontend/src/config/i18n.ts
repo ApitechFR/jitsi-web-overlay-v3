280c2f68075e1c58532a46181cfb5f359a315cc8
Florent Tirvaudey <florentt@apitech.fr>
2026-02-03 16:28:04 +0100

    fix: add ICU to fix grammatical error in conf message homepage

--- jitsi-web-overlay-frontend/src/config/i18n.ts
+++ jitsi-web-overlay-frontend/src/config/i18n.ts
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import ICU from 'i18next-icu';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import enTranslations from './locales/en/translation.json';
 import frTranslations from './locales/fr/translation.json';
@@ -8,6 +9,7 @@ import frTranslations from './locales/fr/translation.json';
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
+  .use(ICU)
   .init({
     resources: {
       fr: { translation: frTranslations },
